ada2312a59398411ec383cd120e39eb2b3a4da9b
TylerHoekstra <89994695+TylerHoekstra@users.noreply.github.com>
2023-01-07 14:40:27 -0700

    changes to yt funct

---
+++ transcript.py
@@ -0,0 +1,20 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+def youtubeLinkInput():
+    link = input()
+
+    if link.find("youtube") != -1:
+        yID = link.split('=')[1]
+        yID = yID.split('&')[0]
+    elif link.find("youtu.be") != -1:
+        yID = link.split('be')[1]
+        yID = yID.split('/')[1]
+
+    transcriptWithTime = YouTubeTranscriptApi.get_transcript(yID)
+
+    string = ""
+    for trans in transcriptWithTime:
+        if trans['text'].find('[') == -1:
+            string += trans["text"] + ' '
+
+    return string
